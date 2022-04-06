769e01f0ef22e6c068f26e79e827c3d11fe9ae8d
Prayag Sahoo <prayagsahoo01@gmail.com>
2022-04-06 23:36:23 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Product from "./pages/Product";
+import Navbar from "./components/Navbar";
+
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return <Home/>;
 };
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,77 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://img.freepik.com/free-photo/portrait-expressive-young-woman-with-shopping-bags_1258-27973.jpg?w=996&t=st=1649167471~exp=1649168071~hmac=95c2991182fd2e5474a3e57f923a21cd042391623b2dc67d7284763f8338277d",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 40% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://img.freepik.com/free-photo/portrait-nice-cute-girls-embracing-holding-hands-carrying-new-cool-purchase-isolated-yellow-wall_231208-11819.jpg?w=1060&t=st=1649172698~exp=1649173298~hmac=b37a7d7ff2896189a8fc00f47b7b9b3c4461e31a1f0852450e7db84fdb40882f",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 40% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://img.freepik.com/free-photo/portrait-young-happy-smiling-woman-with-shopping-bags-with-copyspace-blank-empty-area-text-slogan-calling-by-cell-phone-against-red-wall_231208-10778.jpg?t=st=1649172736~exp=1649173336~hmac=52c532767522779fc8d16b7a172417017cb2caefe912ef81b254991aaa81e419&w=1060",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 40% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://www.pngarts.com/files/4/Anklet-PNG-Download-Image.png",
+    },
+    {
+      id:3,
+      img:"https://www.pngarts.com/files/9/Black-Hoodie-Pullover-PNG-Transparent-Photo.png",
+    },
+    {
+      id:4,
+      img:"https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://www.pngarts.com/files/6/Pink-Cowboy-Hat-PNG-Image-Transparent.png",
+    },
+    {
+      id:7,
+      img:"https://www.pngarts.com/files/9/Brown-Louis-Vuitton-Purse-Transparent-Images.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
