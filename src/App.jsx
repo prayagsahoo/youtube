f3ff8c47298c4a6230335dba75ccfc572d3963c1
Prayag Sahoo <prayagsahoo01@gmail.com>
2022-04-07 00:28:22 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import Cart from "./pages/Cart";
-import Home from "./pages/Home";
+/*import Cart from "./pages/Cart";
+
 import ProductList from "./pages/ProductList";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Product from "./pages/Product";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar";*/
 
-
+import Home from "./pages/Home";
 const App = () => {
   return <Home/>;
 };
